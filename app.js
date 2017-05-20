52e1f4e960d32c48d0599283bf64f108542afe1c
celloalex <alexbuchaniec@gmail.com>
2017-05-20 11:17:07 -0500

    controllers are working

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
-angular.module("myApp", ["ngRoute"]);
+var myApp = angular.module('myApp',["ngRoute"]);
 
-angular.module("myApp").config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+var mainController = function($scope){
+
+}
+
+var peopleController = function($scope, $http){
+    $http.get("http://swapi.co/api/people/").then(
+        function(result){
+            $scope.model = result.data;
+        }
+    );
+
+    $scope.getId = function(person){
+        var array = person.url.split("/");
+        return array[array.length - 2];
+    }
+
+}
+
+var personController = function($scope, $http, $location){
+    var path = $location.path();
+    $http.get("http://swapi.co/api/people/").then(
+        function(result){
+            $scope.model = result.data;
+        }
+    );
+
+}
+
+
+
+
+
+
+
+//allows you to go from one page to another
+
+
+myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     //$locationProvider.html5Mode(true);
     $routeProvider.when("/", {
-        templateUrl: "templates/main.html"
+        templateUrl: "templates/main.html",
+        controller: mainController
     }).when("/people", {
-        templateUrl: "templates/people.html"
+        templateUrl: "templates/people.html",
+        controller: peopleController
+    }).when("/person", {
+        templateUrl: "templates/person.html",
+        controller: personController
     }).when("/planets", {
         templateUrl: "templates/planets.html"
     }).when("/films", {
